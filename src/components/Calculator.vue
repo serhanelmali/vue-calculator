7c4f4a9b3edaf89db28e599c04d368e884409d06
serhanelmali <serhanelmali@gmail.com>
2021-10-19 20:36:17 +0300

    first commit

---
+++ src/components/Calculator.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="p-3" style="max-width: 400px;margin: 50px auto; background: #234">
+    <div
+      class="w-full rounded m-1 p-3 text-right lead font-weight-bold text-white bg-vue-dark"
+    >
+      {{ calculatorValue || 0 }}
+    </div>
+
+    <div class="row no-gutters">
+      <div class="col-3" v-for="n in calculatorElements" :key="n">
+        <div
+          class="lead text-white text-center m-1 py-3 bg-vue-dark rounded hover-class"
+        >
+          {{ n }}
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Calculator",
+  props: {
+    msg: String,
+  },
+
+  data() {
+    return {
+      calculatorValue: "",
+      calculatorElements: [
+        "C",
+        "*",
+        "/",
+        "-",
+        7,
+        8,
+        9,
+        "+",
+        4,
+        5,
+        6,
+        "%",
+        1,
+        2,
+        3,
+        "=",
+        0,
+        ".",
+      ],
+    };
+  },
+};
+</script>
+
+<style scoped>
+.bg-vue-dark {
+  background: #31475e;
+}
+
+.hover-class:hover {
+  cursor: pointer;
+  background: #3d5875;
+}
+</style>
